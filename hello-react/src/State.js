07d67875cd40eff7c9c641bee6b4c6ce0d0718c4
CJewon <112820744+CJewon@users.noreply.github.com>
2023-09-13 14:02:10 +0900

    aa

--- hello-react/src/State.js
+++ hello-react/src/State.js
@@ -1,7 +1,13 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function State() {
+
+    const [count, setCount] = useState(0);  // *** 리엑트식 변수(상수) 정해주는 코드 ***   hook(훅) 이라고 한다.
+
   return (
-    <div>State</div>
+    <div>
+        {count}
+        <button onClick={ () => {setCount(count + 10)}}>올려</button>
+    </div>
   )
 }
