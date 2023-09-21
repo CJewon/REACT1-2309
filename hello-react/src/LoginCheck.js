7adbe3b99a51aa91e73a3bcc6f9f4e405baad403
CJewon <112820744+CJewon@users.noreply.github.com>
2023-09-21 14:56:15 +0900

    aaaa

---
+++ hello-react/src/LoginCheck.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function LoginCheck(props) {
+  if (props.로그인여부) {
+    return <p>안녕하세요 회원님</p>;
+  } else {
+    return <p>로그인해주세요.</p>;
+  }
+}
--- hello-react/src/MeatLove.js
+++ hello-react/src/MeatLove.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React from "react";
 
 export default function MeatLove(props) {
   return (
-    <div>{props.고기종류}고기는 너무 맛있어</div>
-  )
+    <div>
+      {props.고기종류}고기는 {props.순서}번째로 너무 맛있어
+    </div>
+  );
 }
